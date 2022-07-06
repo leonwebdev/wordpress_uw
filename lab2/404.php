dfd19f63dc606a1c12dc4d53de9f2170450f2e97
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-06 15:12:15 -0500

    404 done

---
+++ lab2/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="main_wrapper" style="min-height: 500px;">
+
+    <h1>404 - Not Found</h1>
+    <h2>No such results found.</h2>
+    <p>Please try another search.</p>
+
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
