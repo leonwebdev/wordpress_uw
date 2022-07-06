2a4ab46545bdd8f6a821b222aaec8b1ac1b67296
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-06 14:59:38 -0500

    news letter done

--- lab2/page-about-us.php
+++ lab2/page-about-us.php
@@ -1,17 +1,20 @@
 <?php get_header(); ?>
-<h1>About Us</h1>
+<div class="main_wrapper" style="min-height: 500px;">
+    <h1>About Us</h1>
 
-<section class="flex-container" style="flex-flow: nowrap;margin: 2.5em 0;">
-    <div style="max-width: 350px; padding-right: 2rem;">
-        <h1 style="margin-top: 0;">Welcome to my Blog!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At possimus molestiae maiores recusandae eos
-            hic,
-            temporibus tenetur, fugit laboriosam, nemo soluta consequuntur similique quidem sunt minima delectus.
-            Exercitationem, ipsa libero.</p>
-    </div>
-    <div>
-        <img src="http://capstone.leon-web-dev.com/images/posts/1.jpg" class="img-auto-width post_hero_pic">
-    </div>
+    <section class="flex-container" style="flex-flow: nowrap;margin: 2.5em 0;">
+        <div style="max-width: 350px; padding-right: 2rem;">
+            <h1 style="margin-top: 0;">Welcome to my Blog!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At possimus molestiae maiores recusandae eos
+                hic,
+                temporibus tenetur, fugit laboriosam, nemo soluta consequuntur similique quidem sunt minima delectus.
+                Exercitationem, ipsa libero.</p>
+        </div>
+        <div>
+            <img src="./images/posts/1.jpg" class="img-auto-width post_hero_pic">
+        </div>
 
-</section>
+    </section>
+
+</div>
 <?php get_footer(); ?>
