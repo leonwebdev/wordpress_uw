cb7e759f42918b2c9b412123eac54a47041d89ab
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-06 14:49:32 -0500

    page done

--- lab2/page.php
+++ lab2/page.php
@@ -1,21 +1,20 @@
 <?php get_header(); ?>
 <div class="container">
     <div class="sidebar">
-        <?php get_sidebar('posts'); ?>
+        <?php get_sidebar(); ?>
     </div>
     <div class="main">
-        <h1>Recents</h1>
         <?php if (have_posts()) : ?>
         <?php while (have_posts()) : ?>
         <?php the_post(); ?>
 
         <article>
 
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h1><?php the_title(); ?></h1>
 
             <span class="meta">Posted on <?php the_date(); ?> by <?php the_author(); ?></span>
 
-            <?php the_excerpt(); ?>
+            <?php the_content(); ?>
 
         </article>
 
