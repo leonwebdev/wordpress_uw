3ff9dbc582c1f3f2cbd601a6cb607fdca75740ed
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-10 23:37:51 -0500

    header footer revise

--- project/public/wp-content/themes/WP_Project/header.php
+++ project/public/wp-content/themes/WP_Project/header.php
@@ -16,8 +16,11 @@
                 <div id="header_logo" class="col flex-grow-1 flex-lg-grow-0">
                     <img src="<?= get_template_directory_uri() ?>/images/space_debris_logo.png" alt="logo" />
                 </div>
-                <div id="desktop_menu" class="d-lg-flex col align-content-center">
+                <div id="desktop_menu" class="d-lg-flex col align-content-center flex-lg-grow-1">
                     <?php wp_nav_menu(['menu' => 'header_nav_menu']) ?>
+                </div>
+                <div id="utility_menu" class="d-lg-flex col align-content-center flex-lg-grow-0">
+                    <?php wp_nav_menu(['menu' => 'utility_menu']) ?>
                 </div>
                 <div id="mobile_menu_icon" class="col-auto">
                     <img src="<?= get_template_directory_uri() ?>/images/menu-icon.png" alt="mobile-icon" />
