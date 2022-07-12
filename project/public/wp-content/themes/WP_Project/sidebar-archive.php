8f996e8d2c405f128bbd50f03f2cec06854c38c6
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-12 01:13:38 -0500

    search done

--- project/public/wp-content/themes/WP_Project/sidebar-archive.php
+++ project/public/wp-content/themes/WP_Project/sidebar-archive.php
@@ -1,4 +1,10 @@
 <div class="mb-5">
+    <form role="search" method="get" id="searchform" class="searchform" action="/" style="margin-bottom: 1rem;">
+        <div class="container row align-items-center mx-0">
+            <input type="text" value="" name="s" id="s" class="col flex-grow-1 me-3" />
+            <button type="submit" id="searchsubmit" class="btn btn-primary col flex-grow-0">Search</button>
+        </div>
+    </form>
     <div class="p-3 home_card_h mb-4">
         <h2 class="home_card_h">Categories</h2>
     </div>
